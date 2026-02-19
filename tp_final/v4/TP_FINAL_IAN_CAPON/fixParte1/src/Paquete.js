ec03aebc4e3d4dd7bb148de9c1128114166813fc
iancapon <iancaponrusso@gmail.com>
2026-02-19 15:49:12 -0300

    refactorizacion en paqueteActivo

--- tp_final/v4/TP_FINAL_IAN_CAPON/fixParte1/src/Paquete.js
+++ tp_final/v4/TP_FINAL_IAN_CAPON/fixParte1/src/Paquete.js
@@ -1,60 +1,47 @@
+const Consumible = function (montoFinal) {
+    this.montoActual = 0
+    this.monto = () => this.montoActual
+    this.montoTotal = () => montoFinal
+    this.montoRestante = () => montoFinal - this.montoActual
+    this.consumir = (cantidad) => {
+        if (montoFinal - this.montoActual < cantidad) {
+            throw new Error("No se puede consumir esa cantidad.")
+        }
+        this.montoActual += cantidad
+    }
+}
+
 const PaqueteActivo = function (minutos, megabytes, dias, fechaDeActivacion) {
-    this.minutos = minutos
-    this.minutosUsados = 0
-    this.megabytes = megabytes
-    this.megabytesUsados = 0
-    this.dias = dias
-    this.fechaDeActivacion = fechaDeActivacion
+    this.minutos = new Consumible(minutos)
+    this.megabytes = new Consumible(megabytes)
 
     this.activo = () => true
 
     this.estado = function (fechaActual) {
         const milisEnUnDia = 86400000
         return {
-            fechaDeActivacion: this.fechaDeActivacion,
-            minutosRestantes: this.minutos - this.minutosUsados,
-            megabytesRestantes: this.megabytes - this.megabytesUsados,
-            diasHastaVencimiento: this.dias - (fechaActual.getTime() - this.fechaDeActivacion.getTime()) / milisEnUnDia,
+            fechaDeActivacion,
+            minutosRestantes: this.minutos.montoRestante(),
+            megabytesRestantes: this.megabytes.montoRestante(),
+            diasHastaVencimiento: dias - (fechaActual.getTime() - fechaDeActivacion.getTime()) / milisEnUnDia,
         }
     }
 
     this.vencido = function (fechaActual) {
         const milisEnUnDia = 86400000
-        return fechaActual - this.fechaDeActivacion > dias * milisEnUnDia
+        return fechaActual - fechaDeActivacion > dias * milisEnUnDia
     }
 
     this.agotado = function () {
-        return this.minutos - this.minutosUsados <= 0 && this.megabytes - this.megabytesUsados <= 0
+        return this.minutos.montoRestante() <= 0 && this.megabytes.montoRestante() <= 0
     }
 
     this.consumir = function (resumen) {
-        this.minutosUsados += resumen.minutos
-        this.megabytesUsados += resumen.megabytes
+        this.minutos.consumir(resumen.minutos)
+        this.megabytes.consumir(resumen.megabytes)
     }
 }
-/*
-PaqueteActivo.prototype.activo = () => true
 
-PaqueteActivo.prototype.estado = function (fechaActual) {
-    const milisEnUnDia = 86400000
-    return {
-        minutosRestantes: this.minutos - this.minutosUsados,
-        megabytesRestantes: this.megabytes - this.megabytesUsados,
-        diasHastaVencimiento: this.dias - (fechaActual.getTime() - this.fechaDeActivacion.getTime()) / milisEnUnDia,
-    }
-}
-PaqueteActivo.prototype.vencido = function (fechaActual) {
-    const milisEnUnDia = 86400000
-    return fechaActual - this.fechaDeActivacion > dias * milisEnUnDia
-}
-PaqueteActivo.prototype.agotado = function () {
-    return this.minutos - this.minutosUsados <= 0 && this.megabytes - this.megabytesUsados <= 0
-}
-PaqueteActivo.prototype.consumir = function (resumen) {
-    this.minutosUsados += resumen.minutos
-    this.megabytesUsados += resumen.megabytes
-}
-*/
 const Paquete = function (precio, minutos, megabytes, dias) {
     this.precio = () => precio
     this.minutos = () => minutos
